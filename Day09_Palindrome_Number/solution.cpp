f62be352581e44831fa10bf79d87277da30cb617
Mdraihanahmad <mdraihanahmad47@gmail.com>
2025-05-31 10:31:03 +0530

    Day09-Palindrome_Number

--- Day09_Palindrome_Number/solution.cpp
+++ Day09_Palindrome_Number/solution.cpp
@@ -3,6 +3,7 @@ public:
     bool isPalindrome(int x) {
         // Negative numbers and numbers ending in 0 (but not 0 itself) can't be palindromes
         if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        
         int reversed = 0;
         while (x > reversed) {
             reversed = reversed * 10 + x % 10;
